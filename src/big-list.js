f385d3bfb599a8a5f8963befa09fa828bac064f7
Martin Gustafsson <gusten86@gmail.com>
2015-03-24 15:37:12 +0100

    Add names

--- src/big-list.js
+++ src/big-list.js
@@ -2,15 +2,26 @@ export class BigList{
 
    constructor() {
     this.items = [];
+    this.repeatItems = [];
+    this.numberOfItems = 1000000;
+    this.numberOfRepeatItems = 1000;
   }
 
   activate(){
     // Randomly create a bunch of test data.
     var tlds = ['.com', '.net', '.org', '.edu', '.co.uk', '.io'];
     var domains = ['google', 'facebook', 'yahoo', 'apple', 'youtube', 'amazon', 'aurelia'];
+    var firstNames = ['Shuhel', 'Ashley', 'Mike', 'Matthew', 'Rob', 'Jeremy', 'Martin', 'Patrick', 'Scott', 'Timothy', 'Vildan', 'Scott'];
+    var lastNames = ['Ahmed', 'Grant', 'Graham', 'James', 'Eisenberg', 'Van der Haegen', 'Danyow', 'Gustafsson', 'Walters', 'Criswell', 'Moran', 'Softic', 'Allan'];
     var items = [];
-    for (var i = 0; i < 1000; ++i) {
-      this.items.push(i + ' ' + this.randPick(domains) + this.randPick(tlds));
+
+    var item = '';
+    for (var i = 0; i < this.numberOfItems; ++i) {
+      item = `${i} ${this.randPick(firstNames)} ${this.randPick(lastNames)}`;
+      this.items.push(item);
+      if(i <= this.numberOfRepeatItems){
+        this.repeatItems.push(item);
+      }
     }
   }
 
